1492959edb7b0a4a364858c3638bcc40c111d549
Alex Trottier <postables@rtradetechnologies.com>
2020-02-03 14:32:46 -0800

    more learning

--- ch5/cli/main.c
+++ ch5/cli/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+
 /*
     (i < argc -1) ? " " : ""
     the above says "if i is less than argc-1 print a whitespace, otherwise no"
-*/
 int main(int argc, char *argv[]) {
     int i;
     for (i = 1; i < argc; i++) {
@@ -12,3 +13,14 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+*/
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        int val = strcmp(argv[i], "dog");
+        printf("matched: %d\n", val);
+     //   printf("%s%s", argv[i], (i < argc-1) ? " " : "");
+    }
+    printf("\n");
+    return 0;
+}
---
+++ ch5/pmath/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, int *argv[]) {
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *pa = numbers;
+    printf("%d", *pa); // print 1
+    printf("%d", *(pa+1)); // print 2
+    printf("%d\n", *(pa+2)); // print 3
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", *(pa+i));
+    }
+    *(pa+1) = 69; // set the 2nd element to 69, overridden from 2
+    if (*(pa+1) != 69) {
+        printf("expected 69 got %d\n", *(pa+1));
+        return 1;
+    }
+    printf("%d\n", *pa+1); // this prints 2???
+    printf("%d\n", *(pa+1)); // this prints 69??
+    return 0;
+}
