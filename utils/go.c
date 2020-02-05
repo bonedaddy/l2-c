7ac8f42c68fc7d214504656b1e4b84dc496f182e
Alex Trottier <postables@rtradetechnologies.com>
2020-02-04 20:31:28 -0800

    works

--- utils/go.c
+++ utils/go.c
@@ -1,6 +1,14 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+struct go_goroutine_future *go_goroutine_fut(
+    pthread_t *thread, 
+    pthread_attr_t *attr,
+    void *arg, 
+    struct go_goroutine_future *fut
+);
 
 typedef struct go_thread_data {
     int tid;
@@ -13,10 +21,13 @@ void *go_process_func(void *arg) {
     pthread_exit(NULL);
 }
 
-int go_goroutine(pthread_t *thread, pthread_attr_t *attr, void *arg) {
+int go_goroutine(
+    pthread_t *thread, 
+    pthread_attr_t *attr,
+    void *arg
+) {
     return pthread_create(thread, attr, go_process_func, arg);
 }
-
 
 /*
 if we do not define like this and instead do
@@ -31,8 +42,26 @@ we will get an incomplete type error
 typedef struct go_goroutine_future{
     char *returnValue;
     int exitCode;
+    bool ready, fired;
     pthread_mutex_t mutex;
 }  go_goroutine_future;
+
+
+struct go_goroutine_future *fire_go_goroutine_fut(
+    pthread_t *thread, 
+    pthread_attr_t *attr,
+    void *arg, 
+    void *(execute_func)(void *),
+    struct go_goroutine_future *fut
+) {
+    int response = pthread_create(thread, attr, execute_func, arg);
+    pthread_mutex_lock(&fut->mutex);
+    fut->exitCode = response;
+    fut->returnValue = (char*)&response;
+    fut->fired = true;
+    pthread_mutex_unlock(&fut->mutex);
+    return fut;
+}
 
 
 struct go_goroutine_future *new_go_goroutine_future() {
@@ -44,10 +73,36 @@ struct go_goroutine_future *new_go_goroutine_future() {
     (*fut).returnValue = returnValue;
     (*fut).exitCode = exitCode;
     (*fut).mutex = mutex;
+    (*fut).ready = true;
     return fut;
 }
 
+int main() {
+    struct go_goroutine_future *gr1 = new_go_goroutine_future();
+    struct go_goroutine_future *gr2 = new_go_goroutine_future();
+    struct go_goroutine_future futures[2] = {gr1, gr2};
+    pthread_t threads[2];
+    go_data threads_data[2];
+    for (int i = 0; i < 2; ++i) {
+        threads_data[i].tid = i;
+        fire_go_goroutine_fut(
+            &threads[i],
+            NULL,
+            &threads_data[i],
+            go_process_func,
+            gr1
+        );
+    }
+    for (int i = 0; i < 2; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("exit code: %d\n", (*gr1).exitCode);
+    printf("fired: %d\n", (*gr1).fired);
+    return 0;   
+}
 
+
+/*
 int main(int argCount, char *args[]) {
     new_go_goroutine_future();
     pthread_t threads[2];
@@ -73,4 +128,4 @@ int main(int argCount, char *args[]) {
         pthread_join(threads[i], NULL);
     }
     return 0;
-}
+}*/
